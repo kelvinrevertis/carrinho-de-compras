b518bbc30fb35a5ab438c22252b9ddbb1df9b6b2
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-01-30 22:19:57 -0300

    Comsume API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
+import React from "react";
+import { api } from "./api/axios";
 
-export default function Home() {
+export default function Home({ data }) {
+  const products = data.products;
+
   return (
     <>
-    <h1>Hello World</h1>
+      <h1>Produtos:</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.id}: {product.name}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const { data } = await api.get("products?page=1&rows=8&sortBy=id&orderBy=ASC");
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
