bf6df3cabcca621406470e8f5a3949dd189cdb08
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-02-01 01:50:42 -0300

    Import weight font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
+import { GlobalStyle } from '@/styles/global'
 import { Montserrat } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
-const montserrat = Montserrat({ subsets: ['latin'], weight: ["600", "300"] })
+const montserrat = Montserrat({ subsets: ['latin'], weight: ["300","400","600","700"], style:['normal'] })
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <main className={montserrat.className}><Component {...pageProps} /></main>
+    <>
+      <GlobalStyle/>
+      <main className={montserrat.className}><Component {...pageProps} /></main>
+    </>
+
 
   )
 }
