043f03ae52158910586d99c4479a10067e9e8ad2
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-02-01 01:50:12 -0300

    Create Footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+
+display: flex;
+width: 100%;
+height: 34px;
+justify-content: center;
+align-items: center;
+text-align: center;
+font-weight: 400;
+background-color: #EEEEEE;
+`
